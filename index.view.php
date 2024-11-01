86bd3be1b0811890b5fcb6e256b166c13cd3bdfc
abdulkerim <abdulkerimademrcdf@gmail.com>
2024-11-01 00:30:08 -0700

    hero section finished

--- index.view.php
+++ index.view.php
@@ -4,11 +4,98 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>online pharmacy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./assets/style/index.css">
+    <title>MEDIAEVEN</title>
 </head>
 
 <body>
+    <nav>
+        <div id="top">
+            <div id="logo">
+                <img src="./" alt="midick">
+            </div>
+            <div id="links">
+                <span><a href="./">Home</a></span>
+                <span><a href="./">about us</a></span>
+                <span id="sub">subscribe</span>
+                <span>
+                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                </span>
+            </div>
+        </div>
 
+        <div id="bottom">
+            <span>
+                <a href="./">medcines</a>
+            </span>
+            <span>
+                <a href="./">baby care</a>
+            </span>
+            <span>
+                <a href="./">skine care</a>
+            </span>
+            <span>
+                <a href="./">women care</a>
+            </span>
+            <span>
+                <a href="./">medical equipments</a>
+            </span>
+        </div>
+
+    </nav>
+
+    <!-- main -->
+    <main>
+        <section id="sec1">
+            <div id="headline">
+                <h2>your trusted online pharmacy, deliverd to your door step</h2>
+                <p>
+                    quality medicines, Expert advice, and convenience you can count on
+                </p>
+            </div>
+
+            <div id="search">
+                <form action="./index.php" method="get">
+                    <input type="text" name="search" id="searchinp" placeholder="what are you looking for">
+                    <label for="search">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </label>
+                </form>
+            </div>
+
+            <div id="cta">
+                <a href="./">
+                    <button>
+                        shop now
+                    </button>
+                </a>
+            </div>
+        </section>
+
+        <section id="sec2">
+            <div id="headline">
+                <p>become a member today</p>
+                <p>
+                    get discount benefits
+                </p>
+                <p>
+                    get medicen refill every month
+                </p>
+            </div>
+            <div id="">
+                <p>
+                    save 5% on every order,save 20% on daily essentials
+                </p>
+                <button id="sub">
+                    Register now
+                </button>
+            </div>
+            <div id="familypic">
+                <img src="./assets/pics/PlusFamily.png" alt="become a member">
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
